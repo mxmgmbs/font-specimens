aaf9fba49dbb33a08c1e3dfdb34f9da4bb3def2d
mxmgmbs <mxmgmbs@live.com>
2014-10-04 20:42:28 +0200

    Rought drafts about a website from the book

---
+++ wsite/test1/yolo/js/pages/filtering.js
@@ -0,0 +1,113 @@
+/**
+ * sorting page
+ */
+
+( function( window, $ ) {
+
+'use strict';
+
+var ID = window.ID;
+
+ID.filtering = function() {
+
+  // demo at the top
+  ( function() {
+  	var qsRegex;
+
+    var $container = $('#filtering-demo .isotope').isotope({
+      itemSelector: '.element-item',
+      layoutMode: 'fitRows',
+      transitionDuration: '0.6s',
+      filter: function() {
+      return qsRegex ? $(this).text().match( qsRegex ) : true;
+    }
+    });
+  
+    var $quicksearch = $('#quicksearch').keyup( debounce( function() {
+    qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+    $container.isotope();
+  }, 200 ) ); });
+  // debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+  var timeout;
+  return function debounced() {
+    if ( timeout ) {
+      clearTimeout( timeout );
+    }
+    function delayed() {
+      fn();
+      timeout = null;
+    }
+    timeout = setTimeout( delayed, threshold || 100 );
+  }
+}
+  
+
+    var filterFns = {
+      // show if number is greater than 50
+      numberGreaterThan50: function() {
+        var number = $(this).find('.number').text();
+        return parseInt( number, 10 ) > 50;
+      },
+      // show if name ends with -ium
+      ium: function() {
+        var name = $(this).find('.name').text();
+        return name.match( /ium$/ );
+      }
+    };
+
+    var filterFnsDisplay = {
+      numberGreaterThan50: "function() {\n  var number = $(this).find('.number').text();\n  return parseInt( number, 10 ) > 50;\n}",
+      ium: 'function() {\n  var name = $(this).find(\'.name\').text();\n  return name.match( /ium$/ );\n}'
+    };
+
+    var $codeDisplay = $('#filtering-demo .code-display code');
+
+    $('#filtering-demo .button-group').on( 'click', 'button', function() {
+      var filterValue = $(this).attr('data-filter');
+      var isoFilterValue = filterFns[ filterValue ] || filterValue;
+      var displayFilterValue = filterFnsDisplay[ filterValue ] || filterValue;
+      $container.isotope({ filter: isoFilterValue });
+      $codeDisplay.displayIsotopeCode( 'filter', displayFilterValue );
+    });
+
+  })();
+
+  // ----- combination filters ----- //
+
+  ( function() {
+
+    var $demo = $('#combination-filters-demo');
+
+    var $container = $demo.find('.isotope').isotope({
+      itemSelector: '.color-shape',
+      columnWidth: 80,
+      transitionDuration: '0.6s'
+    });
+
+    var $codeDisplay = $demo.find('pre code');
+
+    // store filter for each group
+    var filters = {};
+
+    $demo.on( 'click', '.button', function() {
+      var $this = $(this);
+      var $buttonGroup = $this.parents('.button-group');
+      var filterGroup = $buttonGroup.attr('data-filter-group');
+      // set filter for the group
+      filters[ filterGroup ] = $this.attr('data-filter');
+      // combine filters
+      var filterValue = '';
+      for ( var prop in filters ) {
+        filterValue += filters[ prop ];
+      }
+
+      $container.isotope({ filter: filterValue });
+      $codeDisplay.displayIsotopeCode( 'filter', filterValue );
+    });
+
+  })();
+
+};
+
+})( window, jQuery );
